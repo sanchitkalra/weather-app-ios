508428465af80ed985ad82fb128e77655be36892
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-01-03 13:28:42 +0530

    Add location manager

--- WeatherX/ContentView.swift
+++ WeatherX/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  WeatherX
 //
-//  Created by Mohinder Kalra on 03/01/22.
+//  Created by Sanchit Kalra on 03/01/22.
 //
 
 import SwiftUI
---
+++ WeatherX/Managers/LocationManger.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManger.swift
+//  WeatherX
+//
+//  Created by Mohinder Kalra on 03/01/22.
+//
+
+import Foundation
