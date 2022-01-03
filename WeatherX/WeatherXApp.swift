553451150f263f808dab976a897da9336163057d
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-01-03 13:16:31 +0530

    Initial Commit

---
+++ WeatherX/WeatherXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherXApp.swift
+//  WeatherX
+//
+//  Created by Mohinder Kalra on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
